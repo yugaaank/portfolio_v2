47af9ae5f8f16eb01c528061c1ab3dc849728866
yugaaank <yugaankrathore0@gmail.com>
2026-02-18 00:10:57 +0530

    added lenis

---
+++ src/components/SmoothScrolling.jsx
@@ -0,0 +1,12 @@
+
+import { ReactLenis } from 'lenis/react'
+
+function SmoothScrolling({ children }) {
+    return (
+        <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+            {children}
+        </ReactLenis>
+    )
+}
+
+export default SmoothScrolling;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SmoothScrolling from './components/SmoothScrolling'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SmoothScrolling>
+      <App />
+    </SmoothScrolling>
   </StrictMode>,
 )
